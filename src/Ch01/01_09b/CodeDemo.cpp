234a13773f0cd7fd17461f0a4e50da122081c48a
Xornith <162120271+Xornith@users.noreply.github.com>
2025-12-02 16:42:22 +0000

    Updates

--- src/Ch01/01_09b/CodeDemo.cpp
+++ src/Ch01/01_09b/CodeDemo.cpp
@@ -7,7 +7,7 @@
 
 int main(){
     std::string name;
-    std::cout << "Enter your name:" << std::flush;
+    std::cout << "Enter your name: " << std::flush;
     std::cin >> name;
     std::cout << "Nice to meet you, " << name << "!" << std::endl;
 
